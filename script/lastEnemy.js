f8ef05d80cad00f6d6d3357b038c3c98ea3512f3
Sandra Luengo <luchosandra@gmail.com>
2018-11-09 11:32:03 +0100

    arreglos

--- script/lastEnemy.js
+++ script/lastEnemy.js
@@ -60,7 +60,7 @@ LastEnemy.prototype.lastBattle = function (enemyType) {
   
    
     var src=this.arrayAttacks[0].src.substr(40,(this.arrayAttacks[0].src).length-1);
-    
+    console.log(src);
 
     if (src == enemyType) {
         this.arrayAttacks.shift(this.arrayAttacks[0]);
